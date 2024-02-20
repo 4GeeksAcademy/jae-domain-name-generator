4731315dcda303921fa89794dec83f719dbef310
Jaemarlee Goin <150928570+Jaemarlee@users.noreply.github.com>
2024-02-20 03:52:47 +0000

    nested for/of loops added

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //This comment is to prevent the autoformatter from doing stuff for a second.
+  let pronoun = ["the", "our", "her", "my"];
+  let adj = ["incredible", "favorite", "beloved"];
+  let noun = ["tentacle", "wart", "eybrow"];
+  let tld = [".com", ".net", ".org", ".edu"];
+
+  for (let pronoun of pronoun) {
+    for (let adj of adj) {
+      for (let noun of noun) {
+        for (let tld of tld) {
+          console.log([pronoun, adj, noun, tld]);
+        }
+      }
+    }
+  }
 };
