3c158738e53735be44c0d8fe0eceef58d6d9a33c
Jaemarlee Goin <150928570+Jaemarlee@users.noreply.github.com>
2024-02-21 19:14:55 +0000

    final commit/added an 's' to array names

--- src/app.js
+++ src/app.js
@@ -7,15 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //This comment is to prevent the autoformatter from doing stuff for a second.
-  let pronoun = ["the", "our", "her", "my"];
-  let adj = ["incredible", "favorite", "beloved"];
-  let noun = ["tentacle", "wart", "eybrow"];
-  let tld = [".com", ".net", ".org", ".edu"];
+  let pronouns = ["the", "our", "her", "my"];
+  let adjs = ["incredible", "favorite", "beloved"];
+  let nouns = ["tentacle", "wart", "eybrow"];
+  let tlds = [".com", ".net", ".org", ".edu"];
 
-  for (let pronoun of pronoun) {
-    for (let adj of adj) {
-      for (let noun of noun) {
-        for (let tld of tld) {
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
           console.log([pronoun, adj, noun, tld]);
         }
       }
