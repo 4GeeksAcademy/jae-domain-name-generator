41c5c07b2acd31216822b30d4e7845585985c0f4
Jaemarlee Goin <150928570+Jaemarlee@users.noreply.github.com>
2024-03-21 01:03:26 +0000

    Url's concatenated instead of listed as an array

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //This comment is to prevent the autoformatter from doing stuff for a second.
   let pronouns = ["the", "our", "her", "my"];
   let adjs = ["incredible", "favorite", "beloved"];
   let nouns = ["tentacle", "wart", "eybrow"];
@@ -16,7 +15,8 @@ window.onload = function() {
     for (let adj of adjs) {
       for (let noun of nouns) {
         for (let tld of tlds) {
-          console.log([pronoun, adj, noun, tld]);
+          let domain = `${pronoun}${adj}${noun}${tld}`;
+          console.log(domain);
         }
       }
     }
