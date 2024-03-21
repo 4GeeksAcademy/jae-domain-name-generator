7d8d5b26a983bf6f4127556930a594b39bfc6f0f
Jaemarlee Goin <150928570+Jaemarlee@users.noreply.github.com>
2024-03-21 13:03:03 +0000

    URLS now generated in random order

--- src/app.js
+++ src/app.js
@@ -5,20 +5,39 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// Function to shuffle an array using Fisher-Yates algorithm
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 window.onload = function() {
   let pronouns = ["the", "our", "her", "my"];
   let adjs = ["incredible", "favorite", "beloved"];
-  let nouns = ["tentacle", "wart", "eybrow"];
+  let nouns = ["tentacle", "wart", "eyebrow"];
   let tlds = [".com", ".net", ".org", ".edu"];
 
-  for (let pronoun of pronouns) {
-    for (let adj of adjs) {
-      for (let noun of nouns) {
-        for (let tld of tlds) {
-          let domain = `${pronoun}${adj}${noun}${tld}`;
-          console.log(domain);
-        }
-      }
-    }
+  // Shuffle arrays once
+  shuffleArray(pronouns);
+  shuffleArray(adjs);
+  shuffleArray(nouns);
+  shuffleArray(tlds);
+
+  // Get the maximum possible number of combinations
+  let maxCombinations = pronouns.length * adjs.length * nouns.length;
+
+  // Iterate through all possible combinations
+  for (let i = 0; i < maxCombinations; i++) {
+    let pronounIndex = i % pronouns.length;
+    let adjIndex = Math.floor(i / pronouns.length) % adjs.length;
+    let nounIndex =
+      Math.floor(i / (pronouns.length * adjs.length)) % nouns.length;
+    let tldIndex = i % tlds.length;
+
+    let domain = `${pronouns[pronounIndex]}${adjs[adjIndex]}${nouns[nounIndex]}${tlds[tldIndex]}`;
+    console.log(domain);
   }
 };
